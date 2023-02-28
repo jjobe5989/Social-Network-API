2c1a149df5b06662679d317966e42dca37297fee
jjobe5989 <s3thjobe@gmail.com>
2023-02-27 19:46:04 -0600

    added create thought to thought-controller.js

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -17,6 +17,7 @@ const thoughtController = {
         });
     },
 
+//    get one thought
     getThoughtById({ params }, res) {
         Thought.findOne({ _id: params.id })
         .populate({
@@ -34,5 +35,26 @@ const thoughtController = {
             console.log(err);
             res.sendStatus(400);
         });
+    },
+// create thought
+    createThought({ params, body }, res) {
+        Thought.create(body)
+          .then(({ _id }) => {
+           return User.findOneAndUpdate(
+              { _id: body.userId },
+              { $push: { thoughts: _id } },
+              { new: true }
+            );
+          })
+          .then((dbThoughtData) => {
+            if(dbUserData) {
+                return res
+                .status(404)
+                .json({ message: "Thought created, No user with this id!" });
+            }
+
+            res.json({ message: "Thought created" });
+          })
+          .catch((err) => res.json(err));
     },
 }
